6383fdfd46bb7592cf27cbd879fad9bf388ac531
neeten97 <67601702+neeten97@users.noreply.github.com>
2020-07-14 12:50:12 -0600

    Removed example code for the requests module

--- src/app.py
+++ src/app.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from flask_restplus import Api, Resource
-import requests
 
-r = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
-print(r.json())
+api = Api() # Initialize an instance of the Flask RestPLUS API class
+app = Flask(__name__) # Initialize Flask
 
-api = Api()
-app = Flask(__name__)
-
+# Initialize and run the API
 api.init_app(app, version='0.0', title='Caliber Staging Module Backend',
              description='The back end for the Caliber Staging Module')
 
-@api.route('/api')
-@api.doc()
+@api.route('/api') # Route declaration
+@api.doc() # Documentation decorator
 class ApiRoute(Resource):
 
     @api.response(200, 'Success')
