5d5600e820b3f6924a9840ebf8af03ac01996f50
TimothyAL <35358436+TimothyAL@users.noreply.github.com>
2020-07-23 14:23:49 -0700

    work

--- src/router/employees.py
+++ src/router/employees.py
@@ -81,11 +81,12 @@ class EmployeeIdRoute(Resource):
         else: # Assume it it is an email otherwise
             res = assoc_db.read_one_associate_by_query({'email': user_id})
         
-        if res and 'swot' in res: # Replace the ID of the swot with the swot itself in response
+        if res and 'swot' in res and res['swot']: # Replace the ID of the swot with the swot itself in response
             for ind, swot in enumerate(res['swot']):
                 this_swot = SWOT.from_dict(swot_db.read_swot_by_id(res['swot'][ind]))
                 res['swot'][ind] = this_swot.to_dict()
-
+        _log.debug(res)
+        res['end_date'] = converter(res['end_date'])
         return res
 
     @api.doc(body=swot_fields)
