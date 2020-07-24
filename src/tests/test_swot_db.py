05acdca36206d01b954e4bd260a3e144ca4c7a12
Luis Candelaria <rcand001@odu.edu>
2020-07-24 17:27:52 -0400

    a few unit tests

--- src/tests/test_swot_db.py
+++ src/tests/test_swot_db.py
@@ -6,6 +6,27 @@ from src.data.swot_db import create_swot
 class TestDatabase(unittest.TestCase):
     ''' This is the database test suite '''
 
-    def test_create_swot(self):
+    @mock.patch('src.data.associates_db._associates.update_associate_swot')
+    def test_create_swot(self, mock_update):
         ''' This method will test the create_swot function '''
+
+        create_swot('email', 'query_val', {'new_swot': 'new_swot'})
+
+        self.assertFalse(mock_update.called)
+
+        create_swot('query_key', 'query_val', {'strengths': 'new_strengths', 
+                                               'weaknesses': 'new_weaknesses',
+                                               'opportunities': 'new_opportunities',
+                                               'threats': 'new_threats',
+                                               'notes': 'new_notes'})
+
+        self.assertFalse(mock_update.called)
+
+        create_swot('email', 'query_val', {'strengths': 'new_strengths', 
+                                           'weaknesses': 'new_weaknesses',
+                                           'opportunities': 'new_opportunities',
+                                           'threats': 'new_threats',
+                                           'notes': 'new_notes'})
+
+        self.assertFalse(mock_update.called)
         
